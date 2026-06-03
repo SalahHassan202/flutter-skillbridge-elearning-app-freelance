87e8b6cc978b688e1c66d326949f6af5321d809c
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-03 23:42:37 +0300

    feat:add app colors file

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryOrange = Color(0xFFFF6B2C);
+  static const Color primaryBlue = Color(0xFF3B5BDB);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color footerBackground = Color(0xFF3B5BDB);
+  static const Color textDark = Color(0xFF1A1A2E);
+  static const Color textGray = Color(0xFF6B7280);
+  static const Color cardBackground = Color(0xFFF8F9FA);
+  static const Color starColor = Color(0xFFFFB800);
+}
