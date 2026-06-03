1b1e92d60e5780f599a0606c3b98c3dfd58fc3ee
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-03 23:45:03 +0300

    feat: add spacing system

---
+++ lib/core/theme/app_dimensions.dart
@@ -0,0 +1,14 @@
+class AppDimensions {
+  static const double paddingXS = 4.0;
+  static const double paddingS = 8.0;
+  static const double paddingM = 16.0;
+  static const double paddingL = 20.0;
+  static const double paddingXL = 24.0;
+  static const double paddingXXL = 32.0;
+
+  static const double borderRadiusS = 8.0;
+  static const double borderRadiusM = 12.0;
+  static const double borderRadiusL = 16.0;
+
+  static const double buttonHeight = 48.0;
+}
