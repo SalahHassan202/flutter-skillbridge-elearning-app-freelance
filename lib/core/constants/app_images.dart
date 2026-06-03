bcbe4f2e5671192c8f1fb22e0d9c3c7f91cbec9f
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-03 23:44:20 +0300

    feat: add image urls

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,14 @@
+class AppImages {
+  static const String heroImage =
+      'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=800';
+  static const String courseDesign =
+      'https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=600';
+  static const String courseWeb =
+      'https://images.pexels.com/photos/270404/pexels-photo-270404.jpeg?auto=compress&cs=tinysrgb&w=600';
+  static const String courseMarketing =
+      'https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=600';
+  static const String avatar1 =
+      'https://randomuser.me/api/portraits/men/32.jpg';
+  static const String avatar2 =
+      'https://randomuser.me/api/portraits/women/68.jpg';
+}
