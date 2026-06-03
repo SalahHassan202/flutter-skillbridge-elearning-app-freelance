32523028d93f508f080a3a7a646450f45a2dad1b
SalahHassan202 <salahhassan66190@gmail.com>
2026-06-04 00:52:27 +0300

    feat: style skillbridge logo with two colors

--- lib/features/home/widgets/about_section.dart
+++ lib/features/home/widgets/about_section.dart
@@ -1,7 +1,9 @@
+// File: lib/features/home/widgets/about_section.dart
+
 import 'package:flutter/material.dart';
 import '../../../core/theme/app_text_theme.dart';
 import '../../../core/theme/app_dimensions.dart';
-import '../../../core/constants/app_strings.dart';
+import '../../../core/constants/app_colors.dart';
 import '../../../core/utils/responsive_utils.dart';
 
 class AboutSection extends StatelessWidget {
@@ -19,16 +21,53 @@ class AboutSection extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            AppStrings.aboutTitle,
-            style: AppTextTheme.headlineMedium.copyWith(
-              fontSize: isSmall ? 22 : 24,
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Skill',
+                  style: AppTextTheme.headlineMedium.copyWith(
+                    color: AppColors.primaryBlue,
+                    fontSize: isSmall ? 22 : 24,
+                  ),
+                ),
+                TextSpan(
+                  text: 'Bridge',
+                  style: AppTextTheme.headlineMedium.copyWith(
+                    color: AppColors.primaryOrange,
+                    fontSize: isSmall ? 22 : 24,
+                  ),
+                ),
+              ],
             ),
           ),
           const SizedBox(height: AppDimensions.paddingM),
-          Text(
-            AppStrings.aboutDescription,
-            style: AppTextTheme.bodyLarge.copyWith(fontSize: isSmall ? 14 : 16),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text:
+                      'هي منصة تعليمية إلكترونية تقدم كراسات قصيرة وعملية في مجالات التخصص: البرمجة، التنسيق الرقمي وغيرها. تستند كل من مهارات حقيقية من خبرة المثال وتطبيقاتها في مشاريع عملية تنتج لك أموال العمل الحر. ',
+                  style: AppTextTheme.bodyLarge.copyWith(
+                    fontSize: isSmall ? 14 : 16,
+                  ),
+                ),
+                TextSpan(
+                  text: 'افضل',
+                  style: AppTextTheme.bodyLarge.copyWith(
+                    color: AppColors.primaryOrange,
+                    fontWeight: FontWeight.bold,
+                    fontSize: isSmall ? 14 : 16,
+                  ),
+                ),
+                TextSpan(
+                  text: ' منصة هتتعامل معاها',
+                  style: AppTextTheme.bodyLarge.copyWith(
+                    fontSize: isSmall ? 14 : 16,
+                  ),
+                ),
+              ],
+            ),
             textAlign: TextAlign.start,
           ),
         ],
